0fcfe256bdc8c6b24583ec1b9193371a07f57b54
Benjamin Bakle <bbakle@apterainc.com>
2017-09-21 13:23:56 -0400

    dev: Initial Commit

---
+++ AngularTemplate/scripts/controllers/payees-controller.js
@@ -0,0 +1,132 @@
+﻿var app = app || angular.module("app", []);
+app.controller('payeesController', payeesController);
+app.directive('deletePayeeDirective', deletePayeeDirective);
+
+payeesController.$inject = ['payeeService'];
+
+function payeesController(service) {
+    var p = this;
+    p.payees = [];
+    p.selectedPayeePayment = 0;
+    p.selectedPayee = selectedPayee;
+
+    p.newPayee = {};
+    p.newPayee.Name = "";
+    p.newPayee.DueDate = "";
+    p.newPayee.Payment = 0;
+    p.newPayee.Active = true;
+
+    p.editPayee = {};
+    p.editPayee.Name = "";
+    p.editPayee.DueDate = "";
+    p.editPayee.Payment = "";
+    p.editPayee.Active = true;
+
+    p.addPayee = addPayee;
+    p.deletePayee = deletePayee;
+    p.confirmDeletePayeeIsVisible = false;
+    p.deletedPayee = {};
+
+    p.getEditPayee = getEditPayee;
+    p.editedPayee = null;
+
+    p.getPayeesTotal = "";
+
+
+    p.editSelectedPayee = editSelectedPayee;
+
+    init();
+    return p;
+
+    function init() {
+        getPayees();
+    };
+
+    function formatDate(date) {
+        var monthNames = [
+          "January", "February", "March",
+          "April", "May", "June", "July",
+          "August", "September", "October",
+          "November", "December"
+        ];
+        var day = date.getDate();
+        var monthIndex = date.getMonth();
+        var year = date.getFullYear();
+
+        return monthNames[monthIndex] + ' ' + day + ', ' + year;
+    }
+
+    function addPayee() {
+        service.addPayee(p.newPayee).then(function (response) {
+            p.payees.push(response.data);
+            getPayees();
+        });
+    }
+
+    function deletePayee(id) {
+        service.deletePayee(id).then(function (response) {
+            p.deletedPayee = response.data;
+            getPayees();
+        })
+    }
+
+    function editSelectedPayee(payee) {
+        service.editPayee(payee).then(function (response) {
+            p.editedPayee = response.data;
+            getPayees();
+        })
+    }
+
+    function getPayees() {
+        service.getPayees().then(function (response) {
+            p.payees = response.data;
+            getPayeesTotal();
+        });
+    }
+
+    function getEditPayee(id) {
+        service.getPayee(id).then(function (response) {
+            p.editPayee = response.data;
+        });
+    }
+
+    function selectedPayee(id) {
+        service.getPayee(id).then(function (response) {
+            p.selectedPayeePayment = response.data.Payment;
+        })
+    }
+
+    function getPayeesTotal() {
+        var total = 0;
+        for (var i = 0; i < p.payees.length; i++) {
+            var payment = p.payees[i].Payment;
+            total += payment;
+        }
+        p.getPayeesTotal = total;
+    }
+}
+
+function deletePayeeDirective() {
+    return {
+        restrict: 'A',
+        scope: true,
+        template: '<a href="javascript:void(0)" class="btn btn-warning btn-sm" ng-show="deleteIsVisible" ng-click="showConfirmDeletePayee()">Delete</a>' +
+            '<div class="btn-group-vertical" role="group">' +
+            '<a class="btn btn-danger btn-sm" ng-show="confirmDeleteIsVisible" ng-click="close()"><i class="fa fa-times" aria-hidden="true"></i></a>' +
+            '<a class="btn btn-danger btn-sm" ng-show="confirmDeleteIsVisible" ng-click="p.deletePayee(payee.PayeeId)">Yep!</a>' +
+            '</div>',
+        controller: function ($scope) {
+            $scope.confirmDeleteIsVisible = false;
+            $scope.deleteIsVisible = true;
+
+            $scope.showConfirmDeletePayee = function () {
+                $scope.confirmDeleteIsVisible = true;
+                $scope.deleteIsVisible = false;
+            }
+            $scope.close = function () {
+                $scope.confirmDeleteIsVisible = false;
+                $scope.deleteIsVisible = true;
+            }
+        }
+    }
+}
---
+++ AngularTemplate/scripts/controllers/transactions-controller.js
@@ -0,0 +1,111 @@
+﻿/// <reference path="../services/payees-service.js" />
+var app = app || angular.module("app", []);
+app.controller('transactionsController', transactionsController);
+app.directive('checkboxSelectorDirective', checkboxSelectorDirective);
+transactionsController.$inject = ['transactionService'];
+function transactionsController(service) {
+    var t = this;
+
+    t.payees = [];
+    t.transactions = [];
+
+    t.populateTransactionById = populateTransactionById;
+
+    t.upcomingDueDate = upcomingDueDate;
+    t.pastDueDate = pastDueDate;
+
+    t.transaction = {};
+    t.transaction.Description = "";
+    t.transaction.Amount = 0.0;
+    t.transaction.PostDate = new Date();
+    t.transaction.ClearDate = new Date();
+    t.transaction.PayeeId = null;
+    t.transaction.ConfirmationNumber = "";
+    t.transactionPayeePosition = null;
+
+    t.getTransactions = getTransactions;
+    t.addTransaction = addTransaction;
+
+    t.getPayees = getPayees;
+    t.updatePayee = updatePayee;
+    t.updatePayeeDueDate = updatePayeeDueDate;
+    t.addTransactionAndUpdatePayee = addTransactionAndUpdatePayee;
+
+    t.upcomingPayeeOffset = 50;
+
+    init();
+
+    return t;
+
+    function init() {
+        getTransactions();
+        getPayees(t.upcomingPayeeOffset);
+    }
+
+    function populateTransactionById(id) {
+        for (i = 0; i < t.payees.length; i++) {
+            if (t.payees[i].PayeeId == id) {
+                t.transactionPayeePosition = i;
+                t.transaction.PayeeId = t.payees[i].PayeeId;
+                t.transaction.Description = t.payees[i].Name;
+                t.transaction.Amount = t.payees[i].Payment;
+            }
+        }
+    }
+    function updatePayee(payee) {
+        service.editPayee(payee).then(function (response) {
+        })
+    }
+    function getPayees(daysOffset) {
+        service.getPayees(daysOffset).then(function (response) {
+            t.payees = response.data;
+        });
+    }
+    function updatePayeeDueDate() {
+        var payeeDueDate = new Date(t.payees[t.transactionPayeePosition].DueDate);
+        t.payees[t.transactionPayeePosition].DueDate = new Date(payeeDueDate.setMonth(payeeDueDate.getMonth() + 1));
+    }
+    function addTransactionAndUpdatePayee(payeeId) {
+        updatePayeeDueDate();
+        updatePayee(t.payees[t.transactionPayeePosition]);
+        addTransaction();
+        getPayees(t.upcomingPayeeOffset);
+    }
+    function getTransactions() {
+        service.getTransactions().then(function (response) {
+            t.transactions = response.data;
+        });
+    }
+    function addTransaction() {
+        service.addTransaction(t.transaction).then(function (response) {
+            t.transactions.push(response.data);
+            getTransactions();
+        });
+    }
+    function upcomingDueDate(prop) {
+        return function (item) {
+            return new Date(item[prop]) > new Date();
+        }
+    }
+    function pastDueDate(prop) {
+        return function (item) {
+            return new Date(item[prop]) < new Date();
+        }
+    }
+    function addPropertyToList(property, value, list) {
+        angular.forEach(list, function (bill) {
+            bill[property] = value;
+        });
+    }
+
+}
+
+function checkboxSelectorDirective() {
+    return {
+        retrict: 'E',
+        scope: { object: '=' },
+        template: '<input type="checkbox" value="{{object.value}}" name="{{object.name}}" />'
+    }
+}
+
+
